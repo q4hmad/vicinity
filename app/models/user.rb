fedfdd0f7eb2ec3e22fe3ff1d4cdb3b3d7409765
Guest User <Guest@Epicodus-1E.local>
2017-12-19 15:13:03 -0800

    changes to columns in bootstrap, devise issue with seeding resolved

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :services, dependent: :destroy
+  has_many :services
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ Service.destroy_all
 10.times do |index|
   service = Service.create!(name: Faker::Job.title,
                   detail: Faker::Lorem.sentence(20, false, 0).chop,
-                  price: Faker::Commerce.price)
+                  price: Faker::Commerce.price,
+                  )
 end
 p "Created #{Service.count} services."
